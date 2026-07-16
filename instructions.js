8d7724e3043e23156be1b1c1e9e5351954c31f10
Nick Madibekov <alikhanm@stanford.edu>
2026-07-15 18:14:30 -0700

    interface upd

--- instructions.js
+++ instructions.js
@@ -1,11 +1,20 @@
 (function (global) {
   "use strict";
 
-  function page(inner, title) {
+  const HEART = '<svg class="ic-heart" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 21s-6.716-4.297-9.193-8.06C1.07 10.2 1.86 6.9 4.6 5.6 6.6 4.65 8.9 5.2 10.3 6.85L12 8.8l1.7-1.95C15.1 5.2 17.4 4.65 19.4 5.6c2.74 1.3 3.53 4.6 1.793 7.34C18.716 16.703 12 21 12 21z"/></svg>';
+
+  function page(inner, title, icon) {
     return `
-      <div class="instr-wrap">
-        ${title ? `<div class="instr-title">${title}</div>` : ""}
-        <div class="instr-text">${inner}</div>
+      <div class="app-screen">
+        <div class="app-chrome">
+          <div class="app-brand"><span class="brand-badge">${HEART}</span><span class="brand-name">spark</span></div>
+          <div class="app-step">Getting started</div>
+        </div>
+        <div class="app-body" style="text-align:center;">
+          ${icon ? `<div class="instr-icon">${icon}</div>` : ""}
+          ${title ? `<div class="instr-title">${title}</div>` : ""}
+          <div class="instr-text">${inner}</div>
+        </div>
       </div>`;
   }
 
